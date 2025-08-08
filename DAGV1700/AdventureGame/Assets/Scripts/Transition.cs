67967b4ebbae1400d58bd1038876ef94788a9675
Griffin180 <j.griffinsnyder@gmail.com>
2025-08-08 13:56:54 -0600

    final level

--- DAGV1700/AdventureGame/Assets/Scripts/Transition.cs
+++ DAGV1700/AdventureGame/Assets/Scripts/Transition.cs
@@ -5,7 +5,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame() 
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(3);
     }
 
     public void QuitGame() 
