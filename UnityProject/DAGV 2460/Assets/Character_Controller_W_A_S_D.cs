dc42ef0c4bb8ee6202b3b33974c3be34e00186b1
Griffin180 <j.griffinsnyder@gmail.com>
2025-11-11 23:05:55 -0700

    Started on Assassin character and Finished Arena defense

---
+++ UnityProject/DAGV 2460/Assets/Character_Controller_W_A_S_D.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Character_Controller_W_A_S_D : MonoBehaviour
+{
+
+    private CharacterController characterController;
+
+    public float Speed = 5f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        characterController.Move(move*Time.deltaTime*Speed);
+    }
+}
